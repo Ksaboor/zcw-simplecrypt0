525c08a55cfc46f7ba1b6ff310ba770f509c21b0
Khail Saboor <khalilsaboor1@gmail.com>
2018-10-02 12:28:39 -0400

    Initial commit

---
+++ Crypto/src/ROT13Test.java
@@ -0,0 +1,121 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class ROT13Test {
+
+
+    @Test
+    public void rotateStringTest0() {
+        // Given
+        String s1 = "ABCDEF";
+        String s2 = "ABCDEF";
+
+        // When
+        ROT13 cipher = new ROT13();
+        String actual = cipher.rotate(s1, 'A');
+
+        // Then
+        assertTrue(actual.equals(s2));
+    }
+
+    @Test
+    public void rotateStringTest1() {
+        // Given
+        String s1 = "ABCDEF";
+        String s2 = "DEFABC";
+
+        // When
+        ROT13 cipher = new ROT13();
+        String actual = cipher.rotate(s1, 'D');
+
+        // Then
+        assertTrue(actual.equals(s2));
+    }
+
+    @Test
+    public void rotateStringTest2() {
+        // Given
+        String s1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String s2 = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+
+        // When
+        ROT13 cipher = new ROT13();
+        String actual = cipher.rotate(s1, 'N');
+        System.out.println(s1);
+        System.out.println(actual);
+        // Then
+        assertTrue(actual.equals(s2));
+    }
+
+    @Test
+    public void cryptTest1() {
+        // Given
+        ROT13 cipher = new ROT13('a', 'n');
+
+        String Q1 = "Why did the chicken cross the road?";
+        String A1 = "Jul qvq gur puvpxra pebff gur ebnq?";
+
+        String Q2 = "Gb trg gb gur bgure fvqr!";
+        String A2 = "To get to the other side!";
+
+        // When
+        String actual = cipher.encrypt(Q1);
+        System.out.println(actual);
+
+        System.out.println(Q1);
+        System.out.println(A1);
+      //  System.out.println(actual);
+        // Then
+        assertTrue(actual.equals(A1));
+
+        // When
+        String actual2 = cipher.decrypt(Q2);
+        System.out.println(Q2);
+        System.out.println(A2);
+        System.out.println(actual2);
+        // Then
+        assertTrue(actual2.equals(A2));
+    }
+    @Test
+    public void cryptTest2() {
+        // Given
+        ROT13 cipher = new ROT13('a', 'n');
+
+        String Q1 = "Why did the chicken cross the road?";
+        System.out.println(Q1);
+
+        // When
+        String actual = cipher.crypt(cipher.crypt(Q1));
+        System.out.println(actual);
+        // Then
+        assertTrue(actual.equals(Q1));
+    }
+    @Test
+    public void textEncryptFile(){
+        //Given
+        File file = new File("sonnet18.txt");
+        //When
+        String expected = ROT13.encrypt("Shall I compare thee to a summer’s day?\n" +
+                "Thou art more lovely and more temperate:\n" +
+                "Rough winds do shake the darling buds of May,\n" +
+                "And summer’s lease hath all too short a date;\n" +
+                "Sometime too hot the eye of heaven shines,\n" +
+                "And often is his gold complexion dimm'd;\n" +
+                "And every fair from fair sometime declines,\n" +
+                "By chance or nature’s changing course untrimm'd;\n" +
+                "But thy eternal summer shall not fade,\n" +
+                "Nor lose possession of that fair thou ow’st;\n" +
+                "Nor shall death brag thou wander’st in his shade,\n" +
+                "When in eternal lines to time thou grow’st:\n" +
+                "So long as men can breathe or eyes can see,\n" +
+                "So long lives this, and this gives life to thee.");
+
+        String actual = ROT13.textFile("sonnet18.txt");
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+}
